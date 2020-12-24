fd86248b86ce95b90c8faa7e1382578d9c7e2c3f
naldal <cec7378@gmail.com>
2020-12-24 20:16:04 +0900

    리팩토링

--- test/chapter2/ProfileTest.java
+++ test/chapter2/ProfileTest.java
@@ -1,26 +1,45 @@
 package chapter2;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ProfileTest {
 
+    public ProfileTest() {
+        System.out.println("Test instance has created");
+    }
+
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @Before
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
+
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Profile profile = new Profile("Bull Hockey, Inc");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-
+        profile.add(new Answer(question, Bool.FALSE));
+        Criterion criterion = new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch);
         criteria.add(criterion);
 
         boolean matches = profile.matchers(criteria);
 
         assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+        boolean matches = profile.matchers(criteria);
+
+        assertTrue(matches);
     }
 
 }
